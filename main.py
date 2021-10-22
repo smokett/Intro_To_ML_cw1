d5e3b840e1bae3477a2df6faee03cece878919d9
He Liu <heliu@tcd.ie>
2021-10-23 00:14:23 +0100

    Added visualize function and report graph

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 from numpy.random import default_rng
 from Decision_tree import DecisionTree
-from utils import n_fold_split, cross_validation
+import utils
+from utils import n_fold_split, cross_validation, visualize
 from metric import MyMetric
 
 
@@ -11,6 +12,7 @@ def load_data(data_path):
 def train(training_data, n=10, seed=1024, prune=False):
     n_folds = n_fold_split(training_data, n, default_rng(seed=seed))
     tr, metric = cross_validation(DecisionTree, n_folds, prune=prune)
+    #visualize(tr.root,"report.PNG",BOX_SIZE=6)
     return tr, metric
 
 
